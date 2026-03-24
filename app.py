7f60e8f4e6c28a19ac69e6c1dd4d523b372fdc50
ajrb93 <ajbuchanan93@gmail.com>
2026-03-23 21:41:09 -0400

    20260323

--- app.py
+++ app.py
@@ -789,7 +789,10 @@ def create_schedule_results(match_sims,matches,team_ratings,selected_season,sele
     return plot_df
 
 def create_schedule_results_figure(results,selected_team):
-    results['score'] = results.for_score.astype('int').astype('str') + ' - ' + results.against_score.astype('int').astype('str')
+    if for_score.isna():
+        results['score'] = ''   
+    else:
+        results['score'] = results.for_score.astype('int').astype('str') + ' - ' + results.against_score.astype('int').astype('str')
 
     fig_height = max(10, len(results) * 0.2)
     fig, ax = plt.subplots(figsize=(7,fig_height))
