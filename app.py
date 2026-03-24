667796876ec9a32cbe531e1fb5de3746fb06ba5e
ajrb93 <ajbuchanan93@gmail.com>
2026-03-23 21:58:01 -0400

    20260323

--- app.py
+++ app.py
@@ -790,7 +790,8 @@ def create_schedule_results(match_sims,matches,team_ratings,selected_season,sele
 
 def create_schedule_results_figure(results,selected_team):
     results.loc[results.for_score.isna(),'score'] = ''
-    results.loc[~results.for_score.isna(),'score'] = results.for_score.astype('int').astype('str') + ' - ' + results.against_score.astype('int').astype('str')
+    results.loc[~results.for_score.isna(),'score'] = (results[~results.for_score.isna()].for_score.astype('int').astype('str') + ' - ' + 
+                                                      results[~results.against_score.isna()].against_score.astype('int').astype('str'))
 
     fig_height = max(10, len(results) * 0.2)
     fig, ax = plt.subplots(figsize=(7,fig_height))
