d2d274a07c53a747d8650bf24c8a7329c17867da
ajrb93 <ajbuchanan93@gmail.com>
2026-03-24 21:53:38 -0400

    20260324

--- app.py
+++ app.py
@@ -899,12 +899,14 @@ def create_schedule_results_figure(results,selected_team):
 
 def plot_spi_chart(data):
     fig = go.Figure()
-    fig.add_trace(go.Scatter(x=data.Date, y=np.where(data.C >= 0.5, data.C, 0.5),fill='tonexty',mode='none',fillcolor='rgba(0, 180, 0, 0.4)',showlegend=False))
+    fig.add_trace(go.Scatter(x=data.Date, y=[0.5] * len(data.Date),fill=None,mode='none',showlegend=False))
     fig.add_trace(go.Scatter(x=data.Date, y=np.where(data.C <  0.5, data.C, 0.5),fill='tonexty',mode='none',fillcolor='rgba(220, 0, 0, 0.4)',showlegend=False))
     fig.add_trace(go.Scatter(x=data.Date, y=[0.5] * len(data.Date),fill=None,mode='none',showlegend=False))
+    fig.add_trace(go.Scatter(x=data.Date, y=np.where(data.C >= 0.5, data.C, 0.5),fill='tonexty',mode='none',fillcolor='rgba(0, 180, 0, 0.4)',showlegend=False))
     fig.add_trace(go.Scatter(x=data.Date, y=data.C,mode='lines',line=dict(color='white', width=1.5),showlegend=False))
     fig.add_hline(y=0.5, line_dash='dash', line_color='gray', line_width=1)
-    fig.update_layout(height=200,margin=dict(l=0, r=0, t=0, b=0),yaxis=dict(range=[0, 1]),plot_bgcolor='rgba(0,0,0,0)',paper_bgcolor='rgba(0,0,0,0)')
+    fig.update_layout(height=200,margin=dict(l=0, r=0, t=0, b=0),yaxis=dict(range=[0, 1],tickvals=[i/10 for i in range(0, 11)],tickformat='.0%'),
+                      plot_bgcolor='rgba(0,0,0,0)',paper_bgcolor='rgba(0,0,0,0)')
     return fig
 
 standings = pd.read_feather('data/standings.ftr')
